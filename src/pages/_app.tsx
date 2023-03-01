ec48d1e2b3556a836af5febe73678e7230ef8f1d
Bernardo Schmitz <bernardo_schmitz@live.com>
2023-03-01 09:04:53 -0300

    toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import Head from 'next/head';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Bernardo Schmitz</title>
       </Head>
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
