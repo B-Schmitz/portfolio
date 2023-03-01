b34c4c357176f0f2a63925b206dee171762fecbf
Bernardo <bernardoschmitz0@gmail.com>
2023-03-01 07:59:27 -0300

    title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Head>
+ <title>Bernardo Schmitz</title>
   return <Component {...pageProps} />;
+  </Head>
 }
