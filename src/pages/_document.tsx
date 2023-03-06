71aba29b264ce05cfdcf9598141cd380162a353d
B-Schmitz <bernardoschmitz0@gmail.com>
2023-03-06 19:50:30 -0300

    meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='pt-BR'>
       <Head />
+      <meta name='author' content='Bernardo Schmitz dos Santos' />
+      <meta name='keywords' content='portfólio, web, desenvolvimento, next js, react, typescript, frontend' />
+      <meta name='description' content='Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. ' />
+      <meta httpEquiv='cache-control' content='no-cache' />
+      <meta httpEquiv='content-type' content='text/html; charset=UTF-8' />
+      <meta name='copyright' content='© 2023 Bernardo Schmitz' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <body>
         <Main />
         <NextScript />
