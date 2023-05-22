a15f74e33f5f3e5daadb679719ff6f2ee53cdc9c
Bernardo Schmitz <bernardo_schmitz@live.com>
2023-05-22 10:41:44 -0300

    meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,31 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='pt-BR'>
-      <Head />
-      <meta name='author' content='Bernardo Schmitz dos Santos' />
-      <meta name='keywords' content='portfólio, web, desenvolvimento, next js, react, typescript, frontend' />
-      <meta name='description' content='Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. ' />
-      <meta httpEquiv='cache-control' content='no-cache' />
-      <meta httpEquiv='content-type' content='text/html; charset=UTF-8' />
-      <meta name='copyright' content='© 2023 Bernardo Schmitz' />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <Head>
+        <meta name='author' content='Bernardo Schmitz dos Santos' />
+        <meta name='keywords' content='portfólio, web, desenvolvimento, next js, react, typescript, frontend' />
+        <meta httpEquiv='cache-control' content='no-cache' />
+        <meta httpEquiv='content-type' content='text/html; charset=UTF-8' />
+        <meta name='copyright' content='© 2023 Bernardo Schmitz' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
+        <title>Bernardo Schmitz</title>
+        <meta name="description" content="Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. " />
+        <meta itemprop="name" content="Bernardo Schmitz" />
+        <meta itemprop="description" content="Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. " />
+        <meta itemprop="image" content="https://raw.githubusercontent.com/B-Schmitz/portfolio/main/public/images/cover.webp" />
+
+        <meta property="og:url" content="https://bernardoschmitz.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Bernardo Schmitz" />
+        <meta property="og:description" content="Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. " />
+        <meta property="og:image" content="https://raw.githubusercontent.com/B-Schmitz/portfolio/main/public/images/cover.webp" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Bernardo Schmitz" />
+        <meta name="twitter:description" content="Conheça mais sobre mim e alguns de meus projetos exibidos nessa página. " />
+        <meta name="twitter:image" content="https://raw.githubusercontent.com/B-Schmitz/portfolio/main/public/images/cover.webp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
