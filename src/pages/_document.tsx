eb0ca71b56537da6eb110d7d5264f20d0d7d19d1
Bernardo <bernardoschmitz0@gmail.com>
2024-07-14 01:10:06 -0300

    fix meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { theme } from 'theme/theme';
-import { TITLE, DESCRIPTION, COVER_IMG, URL, AUTHOR, COPYRIGHT, KEYWORDS } from 'utils';
+import { Html, Head, Main, NextScript } from "next/document";
+import { theme } from "theme/theme";
+import {
+	TITLE,
+	DESCRIPTION,
+	COVER_IMG,
+	URL,
+	AUTHOR,
+	COPYRIGHT,
+	KEYWORDS,
+} from "utils";
 
 export default function Document() {
-  return (
-    <Html lang='pt-BR'>
-      <Head>
-        <meta content={DESCRIPTION} property='description' />
-        <meta content={TITLE} property='og:title' />
-        <meta content={DESCRIPTION} property='og:description' />
-        <meta content={COVER_IMG} property='og:image' />
-        <meta content={TITLE} property='twitter:title' />
-        <meta content={DESCRIPTION} property='twitter:description' />
-        <meta content={COVER_IMG} property='twitter:image' />
-        <meta content='summary_large_image' property='twitter:card' />
-        <meta content='website' property='og:type' />
-        <meta content={URL} property='og:url' />
-        <meta property='theme-color' content={theme.onBackground} />
-
-        <meta name='author' content={AUTHOR} />
-        <meta name='keywords' content={KEYWORDS.join(',')} />
-        <meta httpEquiv='content-type' content='text/html; charset=UTF-8' />
-        <meta name='copyright' content={COPYRIGHT} />
-
-        <link rel='icon' type='image/x-icon' href='/favicon.ico'></link>
-        <link rel='shortcut icon' href='/favicon.ico'></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="pt-BR">
+			<Head>
+				<title>Bernardo Schmitz</title>
+				<meta name="author" content={AUTHOR} />
+				<meta name="copyright" content={COPYRIGHT} />
+				<meta name="keywords" content={KEYWORDS.join(",")} />
+				<meta name="theme-color" content={theme.background} />
+				<meta property="og:title" content={TITLE} />
+				<meta name="description" content={DESCRIPTION} />
+				<meta property="og:url" content={URL} />
+				<meta property="og:type" content="website" />
+				<meta property="og:description" content={DESCRIPTION} />
+				<meta property="og:image" content={COVER_IMG} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta property="twitter:domain" content={URL} />
+				<meta property="twitter:url" content={URL} />
+				<meta name="twitter:title" content={TITLE} />
+				<meta name="twitter:description" content={DESCRIPTION} />
+				<meta name="twitter:image" content={COVER_IMG} />
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,7 +1,16 @@
-export const TITLE = 'Bernardo Schmitz';
-export const AUTHOR = 'Bernardo Schmitz dos Santos';
-export const DESCRIPTION = 'Conheça mais sobre mim, meus projetos e experiências profissionais';
-export const URL = 'https://bernardoschmitz.vercel.app/';
-export const COVER_IMG = '/images/cover.webp';
-export const COPYRIGHT = '© ' + new Date().getFullYear + 'Bernardo Schmitz';
-export const KEYWORDS = ['portfólio', 'web', 'desenvolvimento', 'next js', 'react', 'typescript', 'frontend'];
+export const TITLE = "Bernardo Schmitz";
+export const AUTHOR = "Bernardo Schmitz dos Santos";
+export const DESCRIPTION =
+	"Conheça mais sobre mim, meus projetos e experiências profissionais";
+export const URL = "https://bernardoschmitz.vercel.app/";
+export const COVER_IMG = "https://bernardoschmitz.vercel.app/images/cover.webp";
+export const COPYRIGHT = `© ${new Date().getFullYear}Bernardo Schmitz`;
+export const KEYWORDS = [
+	"portfólio",
+	"web",
+	"desenvolvimento",
+	"next js",
+	"react",
+	"typescript",
+	"frontend",
+];
